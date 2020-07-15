a9b16c85492ae4dfa5bf525cb5c0f22b3f0113ee
akshayshahane <akshay.shahane13@gmail.com>
2020-07-16 01:18:19 +0530

    updated files

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, jsonify
 
-
 '''__name__  special python variable 
 that gives each unique name'''
 
 app = Flask(__name__)
+
+stores = [
+    {
+        'name': 'Store one',
+        'items': [
+            {
+                'name': 'Oreo',
+                'price': '0.99'
+            }
+        ]
+    }
+]
 
 
 # POST to receive data
@@ -31,17 +42,17 @@ def get_store(name):
 # GET /store
 @app.route('/store')
 def get_stores():
-    pass
+    return jsonify({'stores': stores})
 
 
 # POST /store/<string:name>/item {name:,price:}
-@app.route('store/<string:name>/item', methods=['POST'])
+@app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
     pass
 
 
 # GET /store/<string:name>/item
-@app.route('store/<string:name>/item')
+@app.route('/store/<string:name>/item')
 def get_item_in_store(name):
     pass
 
