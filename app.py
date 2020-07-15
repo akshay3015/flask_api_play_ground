1af2296d583ef6c4171e1ee37f03be740e009804
akshayshahane <akshay.shahane13@gmail.com>
2020-07-15 22:03:54 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, jsonify
+
+
+'''__name__  special python variable 
+that gives each unique name'''
+
+app = Flask(__name__)
+
+
+# POST to receive data
+# GET to send data back only
+
+
+@app.route('/')  # home page
+def home():
+    return "Hello World, Bambi here!!"
+
+
+# POST /store data {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+
+# GET /store
+@app.route('/store')
+def get_stores():
+    pass
+
+
+# POST /store/<string:name>/item {name:,price:}
+@app.route('store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+
+# GET /store/<string:name>/item
+@app.route('store/<string:name>/item')
+def get_item_in_store(name):
+    pass
+
+
+app.run(port=5000)
